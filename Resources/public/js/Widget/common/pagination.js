cfe0248f3827951fb889bf66ccdf81466e675999
Raúl Ferràs <raul@viscaweb.com>
2017-02-14 12:29:05 +0100

    Simplify jQuery plugin definition and include only required dependencies instead of the require function.

--- Resources/public/js/Widget/common/pagination.js
+++ Resources/public/js/Widget/common/pagination.js
@@ -1,68 +1,67 @@
-define(['require'], function (require) {
+/* jshint browser:true, node:true, strict:true, noempty:true, noarg:true, eqeqeq:true, browser:true, bitwise:true, curly:true, undef:true, nonew:true, forin:true */
+/* global module:true, define: true, exports: true, console: true */
+define(['jquery'], function ($) {
 
-    var jQuery = require('jquery');
+    $.pagination = function (el, options) {
 
-    (function ($) {
-        $.pagination = function(el, options) {
+        /*
+         * To avoid scope issues, use 'base' instead of 'this'
+         * to reference this class from internal events and functions.
+         */
+        var base = this;
 
-            /*
-             * To avoid scope issues, use 'base' instead of 'this'
-             * to reference this class from internal events and functions.
-             */
-            var base = this;
+        /*
+         * Access to jQuery and DOM versions of element
+         */
+        base.$el = $(el);
+        base.el = el;
 
-            /*
-             * Access to jQuery and DOM versions of element
-             */
-            base.$el = $(el);
-            base.el = el;
+        /*
+         * Initialize the plugin
+         */
+        base.init = function () {
+            base.options = $.extend({}, $.pagination.defaultOptions, options);
+            base.domInit();
+        };
 
-            /*
-             * Initialize the plugin
-             */
-            base.init = function () {
-                base.options = $.extend({}, $.pagination.defaultOptions, options);
-                base.domInit();
-            };
+        /**
+         * Main method binding all the widget box
+         */
+        base.domInit = function () {
+        };
 
-            /**
-             * Main method binding all the widget box
-             */
-            base.domInit = function () {};
-
-            jQuery(el)
-                .on('click', 'li a', function() {
-                    if ($(this).hasClass('disabled') || $(this).hasClass('active')) {
-                        return false;
-                    }
-
-                    base.setFieldValue($(this).attr('data-page'));
-
+        $(el)
+            .on('click', 'li a', function () {
+                if ($(this).hasClass('disabled') || $(this).hasClass('active')) {
                     return false;
-                });
+                }
 
-            base.setFieldValue = function(value) {
-                jQuery("input[id="+base.options.fieldId+"]", el)
-                    .val(value)
-                    .trigger('change');
-            };
+                base.setFieldValue($(this).attr('data-page'));
 
-            base.getValue = function () {
-                return jQuery("input[id="+base.options.fieldId+"]", el).val();
-            };
-
-            // Run initializer
-            base.init();
-        };
-
-        $.pagination.defaultOptions = {
-            fieldId: null
-        };
-
-        $.fn.pagination = function (options) {
-            return this.each(function () {
-                (new $.pagination(this, options));
+                return false;
             });
+
+        base.setFieldValue = function (value) {
+            $("input[id=" + base.options.fieldId + "]", el)
+                .val(value)
+                .trigger('change');
         };
-    }) (jQuery);
+
+        base.getValue = function () {
+            return $("input[id=" + base.options.fieldId + "]", el).val();
+        };
+
+        // Run initializer
+        base.init();
+    };
+
+    $.pagination.defaultOptions = {
+        fieldId: null
+    };
+
+    $.fn.pagination = function (options) {
+        return this.each(function () {
+            (new $.pagination(this, options));
+        });
+    };
 });
